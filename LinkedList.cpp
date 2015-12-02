dc620d92cd37cbfb910ef71d1f3503d333afb153
kunruh9 <kyleunruh@u.boisestate.edu>
2015-12-01 18:34:47 -0700

    attempted definitions of Linked List methods. Still some bugs, but it's a start. Pretty sure there are major memory leaks

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,11 +2,111 @@
 // Created by Kyle Unruh on 11/30/15.
 //
 
-#include "LinkedList.h"
+#include "ListInterface.h"
 
 
 //List method definitions declared in ListInterface
 
-Iterator * LinkedList::iterator() {
-    return new Iterator();
-}
+class LinkedList : public ListInterface {
+
+public:
+    LinkedList() {
+        head = nullptr;
+        listSize = 0;
+    }
+
+    void add(string s) {
+        if (head == nullptr) {
+            head = new node;
+            head->s = s;
+            head->next = nullptr;
+        } else {
+            node* n = new node;
+            n->s = s;
+            n->next = head;
+            head = n;
+        }
+        listSize++;
+    }
+
+    void add(string s, int index) {
+        if (index > listSize)
+            index = listSize;
+
+        if (head == nullptr || index == 0) {
+            return add(s);
+        } else {
+            node* tmp = head;
+            for (int i = 1; i <= index-1; i++) {
+                tmp = tmp->next;
+            }
+            node* n = new node;
+            n->s = s;
+            n->next = tmp->next;
+            tmp->next = n;
+        }
+        listSize++;
+    }
+
+    void clear() {
+        node* nodePtr = head;
+        node* tmp = nullptr;
+        while (nodePtr->next != nullptr) {
+            tmp = nodePtr;
+            nodePtr = nodePtr->next;
+            delete tmp;
+        }
+        head = nullptr;
+    }
+
+    string get(int index) {
+        node* nodePtr = head;
+        while (index--) {
+            nodePtr = nodePtr->next;
+        }
+        return nodePtr->s;
+    }
+
+    bool isEmpty() {
+        return listSize == 0;
+    }
+
+    Iterator* iterator() {
+        return nullptr;
+    }
+
+    string remove(int index) {
+        node* nodeCurr = head;
+        node* nodePrev = head;
+        while (index--) {
+            nodePrev = nodeCurr;
+            nodeCurr = nodeCurr->next;
+        }
+        string s = nodeCurr->s;
+        nodePrev->next = nodeCurr->next;
+//        delete nodeCurr;
+        return s;
+    }
+
+    bool remove(string s) {
+        node* nodePtr = head;
+        while (nodePtr != nullptr) {
+            if (s.compare(nodePtr->s))
+                return true;
+            nodePtr = nodePtr->next;
+        }
+        return false;
+    }
+
+    int size() {
+        return listSize;
+    }
+
+protected:
+    typedef struct node {
+        string s;
+        node* next;
+    };
+    node* head;
+    int listSize;
+};
--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include "LinkedList.h"
-#include "Iterator.h"
+#include "LinkedList.cpp"
 
 using namespace std;
 
 int main() {
 
-    LinkedList* list = new LinkedList();
+    LinkedList list;
+
+    list.add("one");
+    list.add("zero");
+    list.add("hi", 9);
+    list.add("car", 2);
+//    cout << list.remove(1);
+
+    int size = list.size();
+    for (int i = 0; i < size; i++) {
+        cout << "[" << i << "] => \"" << list.get(i) << "\", ";
+    }
+
+    cout << endl;
 
     return 0;
 }
